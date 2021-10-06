83959f48f28724d9ecbe047c6ea9d6d944a717dc
jakuborlinski <j.orlinski@student.utwente.nl>
2021-10-06 18:34:35 +0200

    Add a call to set API key and get articles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NewsService} from "./services/news.service";
+import {tap} from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PUI-News';
+
+  constructor(private newsService: NewsService) {
+    newsService.setAnonymousApiKey();
+
+    newsService.getArticles().subscribe((data) => { console.log(data); });
+  }
 }
